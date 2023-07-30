da6cd0ae58f58e408cae8051361a13a75e41065a
hideo-ichikawa <redzone68000@gmail.com>
2023-07-30 21:41:38 +0900

    first commit

---
+++ src/main/java/com/ichikawa/techpit/chat/chatbachend2/ChatBachend2Application.java
@@ -0,0 +1,13 @@
+package com.ichikawa.techpit.chat.chatbachend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBachend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBachend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ichikawa/techpit/chat/chatbachend2/ChatBachend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichikawa.techpit.chat.chatbachend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBachend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
